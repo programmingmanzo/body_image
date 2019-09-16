174533db9d7ba4601fcd4f6bc363845926209cc3
programmingmanzo <programmingmanzo@gmail.com>
2019-09-16 16:58:33 -0700

    created associations between user and measurement models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password #allows us to use an activerecord method called authenticate.  
 
+    has_many :measurement_entries
+
 end
--- db/migrate/20190916234557_create_measurement_entries.rb
+++ db/migrate/20190916234557_create_measurement_entries.rb
@@ -1,6 +1,8 @@
 class CreateMeasurementEntries < ActiveRecord::Migration
   def change
     create_table :measurement_entries do |t|
+      t.string :measurements 
+      t.integer :user_id 
 
       t.timestamps null: false #This line will automatically gives you time stamp any time you create or update any model
     end
