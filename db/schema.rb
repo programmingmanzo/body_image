4019dcac11ac660299fa7118992f10f74e2885f2
programmingmanzo <programmingmanzo@gmail.com>
2019-09-17 15:41:29 -0700

    updating the merge/updating db

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20190916234557) do
-
-  create_table "measurement_entries", force: :cascade do |t|
-    t.string   "weight"
-    t.string   "hips"
-    t.string   "right_arm"
-    t.string   "left_arm"
-    t.string   "right_thigh"
-    t.string   "left_thigh"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-  end
-=======
 ActiveRecord::Schema.define(version: 0) do
->>>>>>> 0b903ca... created a user model and spec.md file
 
 end
