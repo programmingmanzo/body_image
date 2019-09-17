20a30df60f10c0b6bf8212e68f06ce2ece4783ea
programmingmanzo <programmingmanzo@gmail.com>
2019-09-16 22:23:21 -0700

    added some measurements to my table

--- db/migrate/20190916234557_create_measurement_entries.rb
+++ db/migrate/20190916234557_create_measurement_entries.rb
@@ -1,7 +1,12 @@
 class CreateMeasurementEntries < ActiveRecord::Migration
   def change
     create_table :measurement_entries do |t|
-      t.string :measurements 
+      t.string :weight
+      t.string :hips
+      t.string :right_arm
+      t.string :left_arm
+      t.string :right_thigh
+      t.string :left_thigh   
       t.integer :user_id #Having this line should tell you to add associations between models 
 
       t.timestamps null: false #This line will automatically gives you time stamp any time you create or update any model
