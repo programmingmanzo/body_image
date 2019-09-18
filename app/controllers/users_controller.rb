9e353cb6d7255d4bb5088453448eafa1fd61e58e
programmingmanzo <programmingmanzo@gmail.com>
2019-09-17 23:31:52 -0700

    updated post method for login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,16 @@ class UsersController < ApplicationController
         erb :login
     end
 
-    post '/login' do #receive the login form, find the user and log the user in (create session)
-
+    post '/login' do #receive the login form, find the user and log the user in (create a session)
+        #find the user
+        #params looks like: {email: "user@user.com, password: "password"}
+        @user = User.find_by(email: params[:email])
+        #Authenticate the user - verify the user are who they say they are
+        #they the credentials - email/password combo
+        @user.authenticate(params[:password])
+        #log the user in 
+        #redirect to the user's landing page (show? index? dashboard?)
+       
     end
 
     get '/signup' do 
