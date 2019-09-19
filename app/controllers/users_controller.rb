56fa0fb581eefbbaede897e86ea335a054db99e5
programmingmanzo <programmingmanzo@gmail.com>
2019-09-18 23:12:19 -0700

    db changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,6 @@ class UsersController < ApplicationController
     get '/users/:id' do 
     
         @user = User.find_by(id: params[:id])
-        #binding.pry 
         erb :'/users/show'
     end
 
